a139f39a20c18c9b062b20fd15c964e494a38e1d
hustyutongli <1784979081@qq.com>
2019-03-13 15:54:06 +0800

    第一次作业

---
+++ U201813475-1.16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+/*void comp(int*p,int*q);
+void main()
+{
+    printf("please input three numbers");
+    int x,y,z;
+    scanf("%d %d %d",&x,&y,&z);
+    comp(&x,&y);
+    comp(&y,&z);
+    comp(&x,&y);
+    printf("%d %d %d",x,y,z);
+}
+void comp(int*p,int*q)
+{
+    int i;
+    if(*p<*q)
+    {
+        i=*p;
+        *p=*q;
+        *q=i;
+    }
+}*/
+
